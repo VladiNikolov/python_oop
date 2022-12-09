db5f4aea3b42e6cb20ee1a206ec89da7502faedb
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2022-12-09 16:12:19 +0200

    Create drink_factory.py

---
+++ exam_preparation/oop_exam _15_August_21/project/additional_methods/drink_factory.py
@@ -0,0 +1,13 @@
+from project.drink.tea import Tea
+from project.drink.water import Water
+
+
+class DrinkFactory:
+
+    drink_types = {
+        "Tea": Tea,
+        "Water": Water
+    }
+
+    def create_drink(self, drink_type: str, name: str, portion: int, brand:str):
+        return self.__class__.drink_types[drink_type](name, portion, brand)
