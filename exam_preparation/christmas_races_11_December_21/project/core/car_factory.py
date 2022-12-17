b81da2565867b7af12fa8a609605e6a08b36088b
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2022-12-17 15:18:51 +0200

    Create car_factory.py

---
+++ exam_preparation/christmas_races_11_December_21/project/core/car_factory.py
@@ -0,0 +1,16 @@
+from project.car.muscle_car import MuscleCar
+from project.car.sports_car import SportsCar
+
+
+class CarFactory:
+
+    car_types = {
+        "MuscleCar": MuscleCar,
+        "SportsCar": SportsCar
+    }
+
+    def create_car(self, car_type: str, model: str, speed_limit: int):
+
+        if car_type not in self.car_types:
+            raise RuntimeError("Invalid car type!")
+        return self.car_types[car_type](model, speed_limit)
