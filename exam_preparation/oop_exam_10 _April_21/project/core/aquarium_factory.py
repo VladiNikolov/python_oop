7c0e137056aaf98fd2f137edf07b70f3a855eda9
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-04-10 09:32:41 +0300

    Create aquarium_factory.py

---
+++ exam_preparation/oop_exam_10 _April_21/project/core/aquarium_factory.py
@@ -0,0 +1,16 @@
+from project.aquarium.freshwater_aquarium import FreshwaterAquarium
+from project.aquarium.saltwater_aquarium import SaltwaterAquarium
+
+
+class AquariumFactory:
+
+    aquarium_types = {
+        "FreshwaterAquarium": FreshwaterAquarium,
+        "SaltwaterAquarium": SaltwaterAquarium
+    }
+
+    def create_aquarium(self, aquarium_type: str, aquarium_name: str):
+        if aquarium_type not in self.aquarium_types:
+            raise ValueError(f"Invalid aquarium type.")
+
+        return self.aquarium_types[aquarium_type](aquarium_name)
