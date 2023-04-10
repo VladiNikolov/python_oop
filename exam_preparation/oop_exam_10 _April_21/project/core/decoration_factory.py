c08ed25a4e532e1d81c70a5dd391003f31b51c2c
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-04-10 09:32:38 +0300

    Create decoration_factory.py

---
+++ exam_preparation/oop_exam_10 _April_21/project/core/decoration_factory.py
@@ -0,0 +1,16 @@
+from project.decoration.ornament import Ornament
+from project.decoration.plant import Plant
+
+
+class DecorationFactory:
+
+    decoration_types = {
+        "Ornament": Ornament,
+        "Plant": Plant
+    }
+
+    def create_decoration(self, decoration_type: str):
+        if decoration_type not in self.decoration_types:
+            raise ValueError("Invalid decoration type.")
+
+        return self.decoration_types[decoration_type]()
